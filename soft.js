305fb3096282d319512e912aa6b7885fbcbcbdad
SundarJ <sundarjoshi@hotmail.co.uk>
2015-09-07 19:31:52 +0100

    remove htmlparsing

--- soft.js
+++ soft.js
@@ -23,8 +23,8 @@
     }
 
 
+
     var assert = typeof assert == 'function' ? assert : console.assert.bind(console);
-    var document = root.document;
 
     var lexicon = new Map;
 
@@ -47,55 +47,8 @@
         }, []);
     }
 
-    function initialize() {
-        
-        var softElements = flatten(lexicon.get('elements'));
-        var softAttributes = lexicon.get('attributes');
-        
-        var elementRegex = new RegExp(softElements.map(function (element) {
-            // this is a handy way to create
-            // a HTMLElement regex: just use its .outerHTML;
-            var el = document.createElement(element);
-            el.setAttribute("src", "(\\w(\\\\|/)?)+");
-            var closingTagRegex = new RegExp("<\/" + el.nodeName + ">", "i");
-            return el.outerHTML.replace(closingTagRegex, '');
-        }).join("|"), "g");
-  
-        // test if soft elements are interpreted as text or an element
-        var matches = elementRegex.test(document.body.textContent) ? document.body.textContent : document.body.innerHTML;
-        
-        matches.replace(elementRegex, function (match) {
-            console.log(match);
-        });
-
-        [].forEach.call(document.body.querySelectorAll('*'), function (element) {
-            if (~element.tagName.toLowerCase().indexOf(softElements)) {
-                foundElements.push(element);
-            }
-
-            var attributes = new Map;
-            [].forEach.call(element.attributes, function (attribute) {
-                var name = attribute.nodeName.toLowerCase();
-                var value = attribute.nodeValue.toLowerCase();
-
-                if (~softAttributes.indexOf(name)) {
-                    attributes.set(name, value);
-                }
-            });
-
-            if (attributes.size > 0)
-                foundAttributes.push([element, attributes]);
-        });
-
-    };
-
-    root.foundElements = foundElements;
-    root.foundAttributes = foundAttributes;
-
-    window.addEventListener("DOMContentLoaded", initialize);
-
-
-
+    
+    
     if (typeof define == 'function' && define.amd) {
         define('soft', [], function () {
             return soft;
