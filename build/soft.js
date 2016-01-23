c3b850b64f3b13dfaa49bf94b37d02e2e2b51276
SundarJ <sundarjoshi@hotmail.co.uk>
2016-01-23 15:46:12 +0000

    new build

--- build/soft.js
+++ build/soft.js
@@ -1,41 +1,61 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (factory((global.soft = {})));
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.soft = {})));
 }(this, function (exports) { 'use strict';
 
-    let openTagRE = /<([^ \/]+?)( [^>]+)*?>/g
+  const squotRE = /'/g;
+  const quotRE = /"/g;
+  const lfRE =  /\n/g;
+  const crRE = /\r/g;
+  const slashRE = /\\/g;
+  const lineSepRE = /\u2028/;
+  const paraSepRE = /\u2029/;
 
-    function lex(str) {
-        let ret = []
-        let pos = 0
-        
-        for (let match; match = openTagRE.exec(str);) {
-            ret.push( str.slice(pos, match.index) )
-            ret.push(match)
-            
-            pos = (match.index + match[0].length)
-        }
-        
-        return ret
+  const esc = (s) => {
+      return s
+              .replace(slashRE, '\\\\')
+              .replace(squotRE, '\\\'')
+              .replace(quotRE, '\\"')
+              .replace(lfRE, '\\n')
+              .replace(crRE, '\\r')
+              .replace(lineSepRE, '\\u2028')
+              .replace(paraSepRE, '\\u2029');
     }
 
-    const parse = (str, opts) => {
-        return lex(str)
-    }
+  let openTagRE = /<([^ \/]+?)( [^>]+)*?>/g
 
-    let CACHE = {}
+  function lex(str) {
+      let ret = []
+      let pos = 0
+      
+      for (let match; match = openTagRE.exec(str);) {
+          ret.push( str.slice(pos, match.index) )
+          ret.push(match)
+          
+          pos = (match.index + match[0].length)
+      }
+      
+      return ret
+  }
 
-    const compile = (body) => {
-        let parsed = CACHE[body] || (CACHE[body] = parse(body))
-        
-        return new Function( 'data', `return ${parsed.join('')}` )
-    }
+  const parse = (str, opts) => {
+      return lex(str)
+  }
 
-    const render = (body, data) => compile(body)(data)
+  let CACHE = {}
 
-    exports.parse = parse;
-    exports.compile = compile;
-    exports.render = render;
+  const compile = (body) => {
+      let parsed = CACHE[body] || ( CACHE[body] = parse(body) )
+      parsed = esc( parsed.join('') )
+          
+      return new Function( 'data', `return '${parsed}'` )
+  }
+
+  const render = (body, data) => compile(body)(data)
+
+  exports.parse = parse;
+  exports.compile = compile;
+  exports.render = render;
 
 }));
