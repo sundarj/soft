bd7595a2698425c1030b840cf53f82dccf8536ef
SundarJ <sundarjoshi@hotmail.co.uk>
2016-01-23 15:44:37 +0000

    escape parser output

--- src/compiler.js
+++ src/compiler.js
@@ -1,11 +1,13 @@
 import parse from './parser'
+import { esc } from './util'
 
 let CACHE = {}
 
 const compile = (body) => {
     let parsed = CACHE[body] || ( CACHE[body] = parse(body) )
-    
-    return new Function( 'data', `return ${parsed.join('')}` )
+    parsed = esc( parsed.join('') )
+        
+    return new Function( 'data', `return '${parsed}'` )
 }
 
 export { compile as default }
