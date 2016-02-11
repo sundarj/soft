80876f46345a632b3822e658bec8e360e25ca742
SundarJ <sundarjoshi@hotmail.co.uk>
2016-02-11 01:39:16 +0000

    add lexer tests

--- test/parser.js
+++ test/parser.js
@@ -1,59 +1,130 @@
 const soft = require('..')
 const tap = require('tap')
 
-const data = require('./deps/data')
+const case1 = ' <h1 :is="thing"><!--&self;--><b class="bold">bar</b></h1>'
+const case2 = '<h1 :is="thing" :as="helper"></h1>hello<p class="things-thing" id="things-&self;" :of="things"></p>'
+
+
+console.log('lexer')
+
+tap.deepEqual(
+  soft.lex(case1),
+  [
+    '  ',
+    {
+      t: 'h1',
+      a: {
+        ':is': '"thing"'
+      }
+    },
+    '<!--',
+    {
+      t: 'e',
+    },
+    '-->',
+    {
+      t: 'b',
+      a: {
+        'class': '"bold"'
+      },
+    },
+    'bar',
+    {
+      t: 'c'
+    },
+    {
+      t: 'c'
+    }
+  ]
+)
+
+tap.deepEqual(
+  soft.lex(case2),
+  [
+    {
+      t: 'h1',
+      a: {
+        ':is': '"thing"',
+        ':as': '"helper"',
+      }
+    },
+    {
+      t: 'c'
+    },
+    'hello',
+    {
+      t: 'p',
+      a: {
+        'class': '"things-thing"',
+        'id': '"things-&self;"',
+        ':of': '"things'
+      }
+    },
+    {
+      t: 'c'
+    }
+  ]
+)
+
 
 console.log('parser')
 
 tap.deepEqual(
-    soft.parse('<h1 :is="thing"><!--&self;--><b class="bold">bar</b></h1>'),
+    soft.parse(case1),
     [
-        '<h1',
-        '',
-        '>',
-        '<!--',
-        {
-            i: 'thing'
-        },
-        '-->',
-        '<b class="bold">',
-        'bar',
-        '</b>',
-        '</h1>'
-    ]
+      ' ',
+      {
+        t: 'h1',
+        a: {},
+        c: [
+          '<!--',
+          {
+            i: 'thing',
+          },
+          '-->',
+          {
+            t: 'b',
+            a: {
+              class: 'bold'
+            },
+            c: [
+              'bar'
+            ]
+          }
+        ]
+      }
+  ]
 )
 
 tap.deepEqual(
-    soft.parse('<h1 :is="thing" :as="helper"></h1><p class="things-thing" id="things-&self;" :of="things"></p>'),
+    soft.parse(case1),
     [
-        '<h1',
-        '',
-        '>',
-        {
+      {
+        t: 'h1',
+        a: {},
+        c: [
+          {
             i: 'thing',
             h: 'helper'
-        },
-        '</h1>',
-        '<p class="things-thing" id="things-',
+          }
+        ]
+      },
+      {
+        t: 'p',
+        a: {
+          class: 'things-thing',
+          id: [
+            'things-',
+            {
+              i: 'things'
+            }
+          ]
+      },
+      c: [
         {
-            i: 'things',
-            ix: 0
-        },
-        '"',
-        '>',
-        '<p class="things-thing" id="things-',
-        {
-            i: 'things',
-            ix: 1
-        },
-        '"',
-        '>',
-        '<p class="things-thing" id="things-',
-        {
-            i: 'things',
-            ix: 2
-        },
-        '"',
-        '>'
-    ]
+          i: 'things'
+        }
+      ]
+    }
+  ]
 )
